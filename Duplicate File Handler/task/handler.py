ed57694d46641d22f21e4d3911030e07ea7c86cf
RendelFlegg <rendelflegg@ya.ru>
2022-03-03 17:47:17 +0200

    Completed stage 1

--- Duplicate File Handler/task/handler.py
+++ Duplicate File Handler/task/handler.py
@@ -1,1 +1,15 @@
-# write your code here
+import argparse
+import os
+
+parser = argparse.ArgumentParser(description="Let's parse some directory")
+parser.add_argument('dir', nargs='?', type=str, default=False, help='Enter directory')
+args = parser.parse_args()
+directory = args.dir
+
+if os.path.isdir(directory):
+    for root, dirs, files in os.walk(directory, topdown=False):
+        for name in files:
+            print(os.path.join(root, name))
+else:
+    print('Directory is not specified')
+
