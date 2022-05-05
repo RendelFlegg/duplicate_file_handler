6c58a29cdc36da874db8f7b5e4c1234e028a2383
RendelFlegg <rendelflegg@ya.ru>
2022-05-05 19:15:34 +0300

    Completed stage 4: Added deleting files

--- Duplicate File Handler/task/handler.py
+++ Duplicate File Handler/task/handler.py
@@ -22,6 +22,8 @@ def sorting(dictionary):
 
 
 folder_dictionary = {}
+duplicates = {}
+free_space = 0
 
 if os.path.isdir(directory):
     for root, dirs, files in os.walk(directory, topdown=False):
@@ -46,29 +48,46 @@ for key in keys:
 
 answer = input('Check for duplicates?\n')
 while answer.lower() not in ['yes', 'no']:
-    print('Wrong option')
-    answer = input('Check for duplicates?\n')
+    print('\nWrong option')
+    answer = input('\nCheck for duplicates?\n')
 
 if answer.lower() == 'yes':
     counter = 1
     for key in keys:
-        print(key, 'bytes')
-        list_of_items = folder_dictionary[key]
-        folder_dictionary[key] = {}
-        for item in list_of_items:
-            if item.endswith(f'{file_format}'):
-                item_hash = hashlib.md5()
-                with open(item, "rb") as f:
-                    for chunk in iter(lambda: f.read(4096), b""):
-                        item_hash.update(chunk)
-                if item_hash.hexdigest() not in folder_dictionary[key]:
-                    folder_dictionary[key][item_hash.hexdigest()] = []
-                folder_dictionary[key][item_hash.hexdigest()].append(item)
-        for hash_key in folder_dictionary[key]:
-            if len(folder_dictionary[key][hash_key]) > 1:
-                print(f'Hash: {hash_key}')
-                for item in folder_dictionary[key][hash_key]:
-                    if item.endswith(f'{file_format}'):
-                        print(f'{counter}.', item)
-                        counter += 1
-        print()
+        if len(folder_dictionary[key]) > 1:
+            print(key, 'bytes')
+            list_of_items = folder_dictionary[key]
+            folder_dictionary[key] = {}
+            for item in list_of_items:
+                if item.endswith(f'{file_format}'):
+                    item_hash = hashlib.md5()
+                    with open(item, "rb") as f:
+                        for chunk in iter(lambda: f.read(4096), b""):
+                            item_hash.update(chunk)
+                    if item_hash.hexdigest() not in folder_dictionary[key]:
+                        folder_dictionary[key][item_hash.hexdigest()] = []
+                    folder_dictionary[key][item_hash.hexdigest()].append(item)
+            for hash_key in folder_dictionary[key]:
+                if len(folder_dictionary[key][hash_key]) > 1:
+                    print(f'Hash: {hash_key}')
+                    for item in folder_dictionary[key][hash_key]:
+                        if item.endswith(f'{file_format}'):
+                            duplicates[str(counter)] = {'path': item, 'size': int(key)}
+                            print(f'{counter}.', item)
+                            counter += 1
+            print()
+
+    answer = input('Delete files?\n')
+    while answer.lower() not in ['yes', 'no']:
+        print('\nWrong option')
+        answer = input('\nDelete files?\n')
+    if answer.lower() == 'yes':
+        file_numbers = set(input('\nEnter file numbers to delete:\n').split())
+        while not file_numbers.issubset(set(duplicates.keys())) or not len(file_numbers):
+            print('\nWrong format')
+            file_numbers = set(input('\nEnter file numbers to delete:\n').split())
+        for number in file_numbers:
+            os.remove(duplicates[number]['path'])
+            free_space += duplicates[number]['size']
+        print(f'Total freed up space: {free_space} byte{"s" if free_space > 1 else ""}')
+
